2bb5653f48c1c09f1d3cba5ab5ccd204430b5975
lidawei <lidawei@iciyun.com>
2019-07-03 18:19:48 +0800

    first

--- src/main/java/com/ldw/bootdemo/controller/TestController.java
+++ src/main/java/com/ldw/bootdemo/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.ldw.bootdemo.controller;
 
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
 
+    @Transactional
     @GetMapping("/sw")
     public String test(){
         return  "sw";
