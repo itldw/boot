06cf58158a1b8fc506e179f7affdfe84f42c0d08
itldw <963925626@qq.com>
2019-10-31 18:09:43 +0800

    jj

--- src/main/java/com/example/bootdemo/constroller/SpringController.java
+++ src/main/java/com/example/bootdemo/constroller/SpringController.java
@@ -1,10 +1,16 @@
 package com.example.bootdemo.constroller;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SpringController {
 
+
+    @GetMapping("/hello")
+    public  String hello(){
+        return "hello";
+    }
 
 }
