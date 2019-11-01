e06dfffaaede3fad1d9a6357b77e6ff0075bd363
itldw <963925626@qq.com>
2019-11-01 15:33:38 +0800

    jj

--- src/main/java/com/example/bootdemo/BootdemoApplication.java
+++ src/main/java/com/example/bootdemo/BootdemoApplication.java
@@ -19,26 +19,26 @@ public class BootdemoApplication {
 		SpringApplication.run(BootdemoApplication.class, args);
 
 
-		//bean生命周期
-		BeanFactory bf=new DefaultListableBeanFactory();
-
-		Resource res= (Resource) new ClassPathResource("/car.xml");
-
-		XmlBeanDefinitionReader reader=new XmlBeanDefinitionReader((DefaultListableBeanFactory)bf);
-
-		reader.loadBeanDefinitions(res);
-
-		((DefaultListableBeanFactory)bf).addBeanPostProcessor(new MyBeanPostProcessor());
-		((DefaultListableBeanFactory)bf).addBeanPostProcessor(new MyInstantiationAwareBeanPostProcessor());
-
-		Car car1=(Car)bf.getBean("car");
-		car1.setColor("红色");
-
-		Car car2=(Car)bf.getBean("car");
-
-		System.out.println("car1==car2:"+(car1==car2));
-
-		((DefaultListableBeanFactory)bf).destroySingletons();
+//		//bean生命周期
+//		BeanFactory bf=new DefaultListableBeanFactory();
+//
+//		Resource res= (Resource) new ClassPathResource("/car.xml");
+//
+//		XmlBeanDefinitionReader reader=new XmlBeanDefinitionReader((DefaultListableBeanFactory)bf);
+//
+//		reader.loadBeanDefinitions(res);
+//
+//		((DefaultListableBeanFactory)bf).addBeanPostProcessor(new MyBeanPostProcessor());
+//		((DefaultListableBeanFactory)bf).addBeanPostProcessor(new MyInstantiationAwareBeanPostProcessor());
+//
+//		Car car1=(Car)bf.getBean("car");
+//		car1.setColor("红色");
+//
+//		Car car2=(Car)bf.getBean("car");
+//
+//		System.out.println("car1==car2:"+(car1==car2));
+//
+//		((DefaultListableBeanFactory)bf).destroySingletons();
 	}
 
 }
